c825032a126dbace7a84418cf1e163ec78295004
Mahiamk <mahikomohammed@gmail.com>
2025-05-26 16:26:36 +0800

    Add Lab2.py with various string manipulations and lists

---
+++ AI/Lab/Lab2.py
@@ -0,0 +1,27 @@
+#1
+course="Artificial Intelligence"
+print(course[:3])
+
+#2
+course="Data Sciences"
+print(course[-4:])
+#3
+voice="welcome to ccc1243"
+print(voice.upper())
+
+greeting="hello, may I help you"
+print(greeting.capitalize())
+#5
+AI_app=["ChatGPT","Siri","Alexa","Grok","Llama"]
+for app in AI_app:
+  print("These is one of the worlds best AI apps:", app)
+#6
+country=["Ethiopia","USA","UK","Canada","Australia"]
+for i in country:
+  print(i)
+
+#7
+universities=["Sunway University","Oxford University","UUM","NY"]
+new_university="Albukhary International University"
+universities[2]=new_university
+print(universities)
