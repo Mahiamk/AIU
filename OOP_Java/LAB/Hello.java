7df3d7e049180bd9fa5b7c460f3143ccfb907fe9
Mahiamk <mahikomohammed@gmail.com>
2025-05-26 16:33:28 +0800

    Add Hello.java to print favorite number and greeting message

---
+++ OOP_Java/LAB/Hello.java
@@ -0,0 +1,8 @@
+
+public class Hello {
+  public static void main(String[] args) {
+    int myNu=5;
+    System.out.println("My faviorite number is: "+myNu);
+    System.out.println("Hello World");
+  }
+}
