3a2314c228a0b7ef3d1545f7cc4e6e9fa40cc908
Mahiamk <mahikomohammed@gmail.com>
2025-05-26 16:26:25 +0800

    Add initial Python script with basic programming exercises and comments

---
+++ AI/Lab/lab1.py
@@ -0,0 +1,45 @@
+# Anwar Koji  ID= AIU23102314 group=CS1A_e
+
+# 1. Write a program that prints "Hello, World!" and includes a comment 
+# explaining what the code does.
+print("Hello world!")
+# 2. Write a program that prints "Hello, World!" and add a multi-line comment
+# explaining what your program will do.
+print("Hello world!")
+'''
+This program will print "Hello, World!" to the console.
+'''
+# 3. Create three variables name, age, and language and assign values to them.
+name="Anwar"
+age=21
+language="python"
+# 4. Write three variables using Camel Case, Pascal Case, and Snake Case naming
+# styles.
+nameOfStudent="Anwar" #camel case
+NameOfStudent="Anwar" #pascal case
+name_of_student="Anwar" #snake case
+# 5. Assign three countries to three variables in one line, then print each.
+Ethiopia, USA, Malaysia="Addis Ababa", "Washington D.C", "Kuala Lumpur"
+print(Ethiopia)
+print(USA)
+print(Malaysia)
+# 6. Create a variable for each of the following data types: int, float, str, and bool. 
+# Print each variable along with its type.Create a variable for each of the following data
+#  types: int, float, str, and bool. Print each variable along with its type.
+num=int(5)
+decimal=float(5.9)
+name=str("Anwar")
+isStudent=bool(True)
+print(type(num))
+print(type(decimal))
+print(type(isStudent))
+# 7. Convert an integer to a float, a float to an integer, and a string number to an
+# integer. Print the results.
+num=25
+num=float(num)
+print(num)
+num=int(num)
+print(num)
+num="30"
+num=int(num)
+print(num)
