916deaf526a220f9a1624b8682f418aa1fd279c6
Mahiamk <mahikomohammed@gmail.com>
2025-05-26 16:31:41 +0800

    Add Adress.java with personal address details

---
+++ OOP_Java/LAB/Adress.java
@@ -0,0 +1,9 @@
+public class Adress {
+  public static void main(String[] args){
+    System.out.println("Full name:- Anwar Mohammed Koji");
+    System.out.println("Street:- Jalan Tun Abdul Razak");
+    System.out.println("City:- Alor Setar");
+    System.out.println("State:- Kedah");
+    System.out.println("ZipCode:- 05200");
+  }
+}
