a77428fc1d11e59125501a02d520f1eaf564fb44
Mahiamk <mahikomohammed@gmail.com>
2025-06-02 17:18:10 +0800

    Refactor Array class: move list method call before factorial and add list method for user input

--- OOP_Java/LAB/Array.java
+++ OOP_Java/LAB/Array.java
@@ -13,17 +13,17 @@ public class Array {
     System.out.println("The sum of the array is:"+sum);
     System.out.println("The product of the array is"+product);
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Please enter your number to get the factorial:");
     obj.factorial(scanner);
+    obj.list(scanner);
   }
   public void factorial(Scanner scanner){
+    System.out.println("Please enter your number to get the factorial:");
     int num = scanner.nextInt();
     int fact = 1;
     for (int i=1; i<=num; i++){
       fact *= i;
     }
     System.out.println(fact);
-    scanner.close();
   }
   public void sortArray(int[] arr) {
     for (int i = 0; i < arr.length - 1; i++) {
@@ -38,6 +38,18 @@ public class Array {
     System.out.print("Sorted array: ");
     for (int num : arr) {
       System.out.print(num + " ");
+    }
+    System.out.println();
+  }
+  public void list(Scanner scanner){
+    int [] mylist = new int[10];
+    System.out.println("Enter " + mylist.length + " Values: ");
+    for (int i = 0; i < mylist.length; i++) {
+      mylist[i] = scanner.nextInt();
+    }
+    System.out.print("You entered: ");
+    for (int i = 0; i < mylist.length; i++) {
+      System.out.print(mylist[i] + " ");
     }
     System.out.println();
   }
