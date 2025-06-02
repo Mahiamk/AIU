0e577881159d9a0783859758c3651ac7e151e79b
Mahiamk <mahikomohammed@gmail.com>
2025-06-03 00:02:34 +0800

    Add employe class to calculate net pay based on salary and allowances

---
+++ OOP_Java/Exam/employe.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class employe {
+  public static void main(String[] args){
+    System.out.println("Enter your name: ");
+    Scanner scanner = new Scanner(System.in);
+    String Name = scanner.next();
+    System.out.println("Enter your Salary: ");
+    Double Salary = scanner.nextDouble();
+    Double allowance=Salary*0.1;
+    Double newSalary=Salary+allowance;
+    Double tax=newSalary*0.05;
+    Double netPay=Salary+allowance - tax;
+    System.out.println("The Net pay for "+ Name +" is equal= "+ netPay);
+    scanner.close();
+  }
+}
