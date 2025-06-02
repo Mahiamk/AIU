e0a63052c70b52d1edb9afe4daa5214cbcc8dc31
Mahiamk <mahikomohammed@gmail.com>
2025-06-03 00:03:01 +0800

    Add swamp class to demonstrate variable swapping and output current values

---
+++ OOP_Java/Exam/swamp.java
@@ -0,0 +1,13 @@
+public class swamp {
+  public static void main(String[] args){
+
+    int x=6;
+    int y=8;
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+    System.out.println("Current value of x is: "+x);
+    System.out.println("Current value of y is: "+y);
+  }
+}
